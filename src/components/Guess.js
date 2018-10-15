92661c10124c46e2af9f60c2ed0e928a25bebbfe
Leight Johnson <leightjohnson93@gmail.com>
2018-10-15 10:36:49 -0400

    componentWillMount calculates initial guess

--- src/components/Guess.js
+++ src/components/Guess.js
@@ -2,10 +2,8 @@ import React, { Component } from "react";
 import "../App.css";
 
 const initialState = {
-  min: 1,
-  max: 100,
-  guess: 50,
-  message: "Is it 50?",
+  initialMin: 1,
+  initialMax: 100,
   gameOver: false
 };
 
@@ -15,16 +13,15 @@ class Guess extends Component {
     this.state = initialState;
   }
 
-  lowGuess = () => {
-    const { guess, min } = this.state;
-    let newGuess = Math.round((min + guess - 2) / 2);
-    newGuess = newGuess === 0 ? 1 : newGuess;
-    const message = `Is it ${newGuess}?`;
-    this.setState({ message, guess: newGuess });
-  };
+  componentWillMount() {
+    const { initialMin, initialMax } = this.state;
+    const guess = Math.floor((initialMin + initialMax) / 2);
+    const message = `Is it ${guess}`;
+    this.setState({ guess, message, min: initialMin, max: initialMax });
+  }
 
   checkForWinHigh = () => {
-    let { max, min, guess } = this.state;
+    const { max, min, guess } = this.state;
     if (max - min <= 2) {
       const message = `Your number was ${guess + 1}!`;
       const gameOver = true;
@@ -63,9 +60,12 @@ class Guess extends Component {
 
   lowerClick = () => {
     if (this.checkForWinLow()) return;
-    this.lowGuess();
+    const { guess, min } = this.state;
+    let newGuess = Math.round((min + guess - 2) / 2);
+    newGuess = newGuess === 0 ? 1 : newGuess;
+    const message = `Is it ${newGuess}?`;
     const max = this.state.guess - 1;
-    this.setState({ max });
+    this.setState({ max, message, guess: newGuess });
   };
 
   correctClick = () => {
@@ -79,7 +79,8 @@ class Guess extends Component {
       <div className="Tile">
         <h1>Guess</h1>
         <h3>
-          Think of a number between {this.state.min} and {this.state.max}
+          Think of a number between {this.state.initialMin} and{" "}
+          {this.state.initialMax}
         </h3>
         <h2>{this.state.message}</h2>
         <button
